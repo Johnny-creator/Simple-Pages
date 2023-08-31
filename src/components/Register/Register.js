e0edade43a4f2dc853e83ac80e8076932996b2f2
Jonathan Nanno <jon.nanno@gmail.com>
2023-08-31 15:19:36 -0400

    Removed Form component import, implemented its own content

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,13 +1,65 @@
-import React from "react";
-import Form from '../Form/Form';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Input from "../Form/Input";
+import Button from "../UI/Button";
 
-import './register.css';
+import "./register.css";
 
 const Register = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  const nameChangeHandler = (e) => {
+    setUserName(e.target.value);
+  };
+
+  const passwordChangeHandler = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const confirmPasswordChangeHandler = (e) => {
+    setConfirmPassword(e.target.value);
+  };
   return (
     <main>
       <h1 className="registerHeader"> Create User</h1>
-      <Form />
+      <form>
+        <label> User Name: </label>
+        <Input
+          type="text"
+          name="userName"
+          value={userName}
+          holder="User Name: "
+          onChange={nameChangeHandler}
+        ></Input>
+
+        <label> Password: </label>
+        <Input
+          type="text"
+          name="password"
+          value={password}
+          holder="Password: "
+          onChange={passwordChangeHandler}
+        ></Input>
+
+        <label> Create Password: </label>
+        <Input
+          type="text"
+          name="confirmPassword"
+          value={confirmPassword}
+          holder="Confirm Password: "
+          onChange={confirmPasswordChangeHandler}
+        ></Input>
+        <Button onClick={submitHandler} type="submit">
+          Create User
+        </Button>
+        <Link to="/"> Home</Link>
+      </form>
     </main>
   );
 };
