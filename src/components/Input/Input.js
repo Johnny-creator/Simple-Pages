2cfddd6bebbe741dfa07d2a3ea8011fcb4595a70
Jonathan Nanno <jon.nanno@gmail.com>
2023-09-05 15:17:54 -0400

    Reformatted, added an on blur property

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-import './input.css';
+import "./input.css";
 
 const Input = (props) => {
-  return <input type={props.type} name={props.name} value={props.value} placeholder={props.holder} onChange={props.onChange}></input>;
+  return (
+    <input
+      type={props.type}
+      name={props.name}
+      value={props.value}
+      placeholder={props.holder}
+      onChange={props.onChange}
+      onBlur={props.onBlur}
+    ></input>
+  );
 };
 
 export default Input;
