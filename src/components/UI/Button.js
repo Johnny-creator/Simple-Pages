99618ede119e8a575d8954409cbe51da343ed3a6
Jonathan Nanno <jon.nanno@gmail.com>
2023-09-05 15:14:29 -0400

    Added a disabled prop to the button, with disabled CSS styling

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -3,8 +3,8 @@ import "./button.css";
 
 const Button = (props) => {
   return (
-    <button onClick={props.onClick} type={props.type}>
-      {props.children}
+    <button onClick={props.onClick} disabled={props.disabled} type={props.type}>
+      {props.disabled ? "Fill out information above"  : props.children}
     </button>
   );
 };
