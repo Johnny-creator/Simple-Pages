f12092c55e50cb14fb595558d57c7fd2afe03fc8
Jonathan Nanno <jon.nanno@gmail.com>
2023-09-06 13:23:26 -0400

    Fleshed out the error page and added a stylesheet for it

--- src/components/UI/Error.js
+++ src/components/UI/Error.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Button from "./Button";
+
+import './error.css';
 
 const Error = () => {
-    return(
-        <p> There has been an error </p>
-    )
+  return (
+    <main className="error">
+      <h1>Oops, there's been an error!</h1>
+      <Link to="/">
+        <Button type="button"> Return Home </Button>
+      </Link>
+    </main>
+  );
 };
 
 export default Error;
