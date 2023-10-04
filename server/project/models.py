4ccfb36f18a1fc3a3f7a5574632403778eea8198
Sheldon Codling <codlinsh@gmail.com>
2023-10-04 01:27:09 -0400

    Added a few more rows to the models

--- server/project/models.py
+++ server/project/models.py
@@ -1,23 +1,32 @@
+from sqlalchemy import ForeignKey, Text, BigInteger, Column, Boolean
 from sqlalchemy.orm import relationship
-from sqlalchemy import ForeignKey, Text, BigInteger, Column
+from sqlalchemy.dialects.postgresql import UUID
 from project import db
 from werkzeug.security import generate_password_hash
+import uuid
 
 # USER CLASS
 class User(db.Model):
     __tablename__ = 'users'
 
     id = Column(BigInteger, primary_key=True)
+    site_id = relationship('Site', backref='user', uselist=False)
     username = Column(Text, nullable=False, unique=True)
     password = Column(Text, nullable=False)
     email = Column(Text, nullable=False, unique=True)
-    site_id = relationship('Site', backref='user', uselist=False)
+    activation_UUID = db.Column(UUID(as_uuid=True), default=uuid.uuid4)
+    is_active = Column(Boolean, nullable=False)
+    is_banned = Column(Boolean, nullable=False)
+    
 
     def __init__(self, username, password, email, site_id):
         self.username = username
         self.password = generate_password_hash(password)
         self.email = email
         self.site_id = site_id
+        self.is_active = False
+        self.is_banned = False
+
 
     def __repr__(self):
         return "Username is " + self.username
