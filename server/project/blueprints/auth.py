6d44b6efc765cd5dcf9df9c30d2589fb0576e5ea
Sheldon Codling <codlinsh@gmail.com>
2023-10-29 00:59:06 -0400

    Finished login route, started create_site route

--- server/project/blueprints/auth.py
+++ server/project/blueprints/auth.py
@@ -1,22 +1,39 @@
 from flask import Blueprint, jsonify, request
 from project.models import User, Site
-from flask_jwt_extended import create_access_token, jwt_required, current_user, get_jwt_identity
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, current_user, get_jwt_identity
+from werkzeug.security import check_password_hash
 
 auth_bp = Blueprint("auth", __name__)
 
 @auth_bp.post("/login")
 def login():
-    username = request.json.get("username", None)
-    password = request.json.get("password", None)
+    data = request.get_json()
+    
+    current_user = User.query.filter_by(username=data.get("username")).first()
 
-    print("Username: " + username + " and Password: " + password)
+    if check_password_hash(current_user.password, data.get("password")): 
+        access_token = create_access_token(identity=data.get("username"))
+        refresh_token = create_refresh_token(identity=data.get("username"))
+        return jsonify(
+            {
+                "current_user": data.get("username"),
+                "access_token": access_token,
+                "refresh_token": refresh_token
+            }), 200
 
-    access_token = create_access_token(identity=username)
-    return jsonify(
-        {
-            "current_user": username,
-            "access_token": access_token,
-        }), 200
+    return jsonify({"error": "invalid username or password"}), 418
+
+@auth_bp.post("/make_site")
+@jwt_required
+def make_site():
+    # TODO
+    # - query the users site
+    # - post the data to database
+    # - return message
+    data = request.get_json()
+
+    #current_site = Site.query.filter_by(=data.get)
+
 
 @auth_bp.get("/whoami")
 @jwt_required()
